b1a6a0250d9ae2ff982fb13612c7ab1cf2c48880
EnvDock <envdock@gmail.com>
2026-02-27 17:07:48 +0500

    Update edk.rb

--- Formula/edk.rb
+++ Formula/edk.rb
@@ -1,17 +1,17 @@
 class Edk < Formula
   desc "EnvDock CLI"
   homepage "https://github.com/envdock-spec/edk"
-  version "1.0.0"
+  version "1.1.1"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/envdock-spec/edk/releases/download/v0.1.0/envdock-cli-macos-arm64"
-    sha256 "f874db51873969ef0e11cf209ff65b6cfc60e136e247fee609023765cef273ea"
+    sha256 "1b77c26bb417d80cf807e67b5f11667760b474b83cb9d06a48c891d37b95aea2"
   elsif OS.mac?
     url "https://github.com/envdock-spec/edk/releases/download/v0.1.0/envdock-cli-macos-x64"
-    sha256 "d290c309aaa1bfa396e206774f7d5737c4583f24a64ea792ddad8021236b1312"
+    sha256 "c5c4a3ae65cc59d22216d3e8e30385c2bb09b259869f07f49690905a89a5406f"
   elsif OS.linux?
     url "https://github.com/envdock-spec/edk/releases/download/v0.1.0/envdock-cli-linux-x64"
-    sha256 "9b359ce3e0e6e55ceeda92e7ca916077122526f462fa3496c0b53d95d40505ed"
+    sha256 "d8d275b6e2102976df5e27cd6c1a69572114b78028326ec49f5650fa91ca832f"
   end
 
   def install
